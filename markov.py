9b35e75b417b10a3d6e7de638ddfca551bacd265
Your Name <mahafatima14@gmail.com>
2021-11-03 04:06:06 +0000

    Final commit for the day

--- markov.py
+++ markov.py
@@ -2,6 +2,10 @@
 
 import sys
 from random import choice
+import discord
+import os
+
+
 
 
 def open_and_read_file(filenames):
@@ -64,3 +68,19 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['DISCORD_TOKEN'])
